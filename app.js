c9cc2258a28536cfaa396500abbca3f98de53a96
AakarshPurwar <purwaraakarsh@gmail.com>
2024-04-10 15:20:50 +0530

    dynamic port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const port = 8080;
+const port = process.env.PORT || 8080;
 const path = require("path");
 const methodOverride = require( "method-override" );
 const dotenv = require('dotenv');
